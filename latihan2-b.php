6760ebf0b3b27805910b81145327e7f0600cce67
lutfiainulhakim <114342272+lutfiainulhakim@users.noreply.github.com>
2022-10-04 22:19:23 +0700

    Create latihan2-b.php

---
+++ latihan2-b.php
@@ -0,0 +1,8 @@
+<?php
+require_once (" latihan2-a.php ");
+$ mhs2 = mahasiswa baru (" Jamilatur "," G.211.21.0088 ");
+$ mhs2 -> cetak ();
+
+// // Akan terjadi error karena Contructor dalam php oop tidak bisa di override
+// dan pada sebuah class tidak boleh memiliki lebih dari satu Contructor
+?>
