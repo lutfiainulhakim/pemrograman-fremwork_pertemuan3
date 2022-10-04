20681a17d3631b37e63511be01e7da6bc276b84f
lutfiainulhakim <114342272+lutfiainulhakim@users.noreply.github.com>
2022-10-04 22:26:30 +0700

    Update latihan2-b.php

--- latihan2-b.php
+++ latihan2-b.php
@@ -1,6 +1,6 @@
 <?php
 require_once (" latihan2-a.php ");
-$ mhs2 = mahasiswa baru (" Jamilatur "," G.211.21.0088 ");
+$ mhs2 = mahasiswa baru (" LutfiAinulHakim "," G.211.21.0048 ");
 $ mhs2 -> cetak ();
 
 // // Akan terjadi error karena Contructor dalam php oop tidak bisa di override
