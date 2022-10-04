11556004e1b1216dcf0b966beb61179e242dac6b
lutfiainulhakim <114342272+lutfiainulhakim@users.noreply.github.com>
2022-10-04 22:29:17 +0700

    Create latihan3-b.php

---
+++ latihan3-b.php
@@ -0,0 +1,18 @@
+<?php
+require_once("latihan3-a.php");
+$mhs1=new mahasiswa();
+$mhs1->nama="hendra";
+echo $mhs1->nama;
+
+
+//mencetak isi dari nim dan namadengan modifier private
+
+// require_once("latihan-3.php");
+
+// $mhs1=new mahasiswa();
+// $mhs1->setNama("Nama:Jamilatur R<br>");
+// echo $mhs1->getNama();
+
+// $mhs1->setNim("NIM : G.211.21.0088");
+// echo $mhs1->getNim();
+?>
